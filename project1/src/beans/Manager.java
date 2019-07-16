047ec60590081f7d73262e17ec2107acaf21ffbd
McDesmond <mcdesmondd@gmail.com>
2019-07-16 10:59:34 -0400

    Project 1
    Project One so far

---
+++ project1/src/beans/Manager.java
@@ -0,0 +1,26 @@
+package beans;
+
+public class Manager {
+
+	public Manager(String username, String password) {
+		// TODO Auto-generated constructor stub
+		this.username = username;
+		this.password = password;
+	}
+	String username;
+	String password;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}

5a9b834fd43cef06cb4ff1ab48782c8107e91067
McDesmond <mcdesmondd@gmail.com>
2019-07-17 11:18:27 -0400

    updated beans
    beans updated

--- ProjectOne/src/beans/Employee.java
+++ ProjectOne/src/beans/Employee.java
@@ -22,5 +22,38 @@ public class Employee {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Employee [username=" + username + ", password=" + password + "]";
+	}
 	
 }
--- ProjectOne/src/beans/Manager.java
+++ ProjectOne/src/beans/Manager.java
@@ -22,5 +22,38 @@ public class Manager {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Manager other = (Manager) obj;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Manager [username=" + username + ", password=" + password + "]";
+	}
 	
 }

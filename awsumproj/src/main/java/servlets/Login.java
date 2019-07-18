4427b20b11ce84a95781e68be385b2d7138b796c
McDesmond <mcdesmondd@gmail.com>
2019-07-18 11:04:46 -0400

    Created Login Servlet
    Login Serlvet

---
+++ awsumproj/src/main/java/servlets/Login.java
@@ -0,0 +1,18 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class Login extends HttpServlet {
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+		
+	}
+	
+}
